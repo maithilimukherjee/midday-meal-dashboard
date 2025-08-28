0b1e9d28c87ca488048090c37c0608ecad018b0e
Maithili Mukherjee <86041684+maithilimukherjee@users.noreply.github.com>
2025-08-28 23:12:53 +0530

    workingggg

--- src/dashboard.py
+++ src/dashboard.py
@@ -15,3 +15,23 @@ plt.xlabel('State')
 plt.xticks(rotation=45)
 plt.show()
 
+#pie-chart
+def coverage_category(pct):
+    if pct>=90:
+        return 'Excellent'
+    elif pct>=75:
+        return 'Good'
+    elif pct>=50:
+        return 'Average'
+    else:
+        return 'Poor'
+
+df['coverage_category'] = df['coverage_pct'].apply(coverage_category)
+
+category_counts = df['category'].value_counts()
+
+plt.figure(figsize=(6,6))
+category_counts.plot(kind='pie', autopct='%1.1f%%', startangle=140)
+plt.title("Distribution of Schools by Coverage Category")
+plt.ylabel("")
+plt.show()
